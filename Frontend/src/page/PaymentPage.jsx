db23a50b7c9a942a0a1f40edff15aede16b19ab4
WhodisMan <63545109+WhodisMan@users.noreply.github.com>
2024-07-28 06:55:20 +0530

    changes

--- Frontend/src/page/PaymentPage.jsx
+++ Frontend/src/page/PaymentPage.jsx
@@ -1,6 +1,5 @@
-
 import React, { useState, useEffect,Link } from 'react';
-import {Button } from '@mui/material';
+import { Grid, Paper, Typography, Button } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
